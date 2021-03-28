cca041472587f3dcd5fdea6bdd3e6939bcdadcbc
MarkoMilijevic <Marko.Milijevic@ocsbstudent.ca>
2021-03-28 17:05:35 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+myAge = 15
+guess = 0
+
+while (guess != myAge) {
+  guess = prompt('Guess my age between 1 and 20')
+  if (myAge > guess) {
+    alert('Guess a higher number!')
+  } else if (myAge < guess) {
+    alert('Guess a lower number!')
+  } else {
+    alert('You have guessed my age correctly!')
+  }
+}
